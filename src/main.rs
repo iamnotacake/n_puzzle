5465c031924895e3341558875c98e35e0bbd2ec3
Anatoliy Lischynsky <alischyn@student.unit.ua>
2019-04-27 21:58:37 +0300

    Improve impl Display for State

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,37 @@ struct State {
 
 impl fmt::Display for State {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "┌─")?;
-        for _ in 0..self.size {
-            write!(f, "───")?;
-        }
-        writeln!(f, "┐")?;
+        writeln!(
+            f,
+            "┌{}────┐",
+            "────┬".repeat(self.size - 1)
+        )?;
 
         for y in 0..self.size {
-            write!(f, "│ ")?;
             for x in 0..self.size {
                 let number = self.table[y * self.size + x];
+
                 match number {
-                    0 => write!(f, "   ")?,
-                    _ => write!(f, "{:2} ", number)?,
+                    0 => write!(f, "│    ")?,
+                    _ => write!(f, "│ {:2} ", number)?,
                 }
             }
             writeln!(f, "│")?;
+
+            if y < self.size - 1 {
+                writeln!(
+                    f,
+                    "├{}────┤",
+                    "────┼".repeat(self.size - 1)
+                )?;
+            }
         }
 
-        write!(f, "└─")?;
-        for _ in 0..self.size {
-            write!(f, "───")?;
-        }
-        writeln!(f, "┘")?;
+        writeln!(
+            f,
+            "└{}────┘",
+            "────┴".repeat(self.size - 1)
+        )?;
 
         Ok(())
     }
